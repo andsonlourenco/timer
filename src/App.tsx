cce5694a92b341db0809b56729d8b07c077b628b
Andson Lourenco <alourenco@linets.cl>
2022-08-17 10:01:09 -0300

    adding global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./components/styles/global";
 import { defaultTheme } from "./components/styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
